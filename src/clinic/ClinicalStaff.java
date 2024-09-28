2e365342d8534df00d0155bc9867ecf6d04a5f4b
rajiv <rajiv@DESKTOP-2RDGT1I>
2024-09-28 18:35:32 -0400

    Added exceptions to the constructor for ClinicalStaff.

---
+++ src/clinic/ClinicalStaff.java
@@ -0,0 +1,55 @@
+package clinic;
+
+/**
+ * Class that represents a clinical staff member that is working for the clinic.
+ * This class inherits the staff class.
+ */
+public class ClinicalStaff implements Staff {
+  private String firstName;
+  private String lastName;
+  private EducationLevel educationLevel;
+  private String npiLevel;
+	
+  /**
+   * A constructor for the ClinicalStaff class that represents a clinical staff member.
+   * 
+   * @param firstName The first name of the clinical staff member.
+   * @param lastName The last name of the clinical staff member.
+   * @param educationLevel The education level of the clinical staff member.
+   * @param npiLevel The NPI level of the clinical staff member.
+   */
+  public ClinicalStaff(String firstName, String lastName, EducationLevel 
+      educationLevel, String npiLevel) {
+    if (firstName == null || lastName == null || educationLevel == null || npiLevel == null) {
+      throw new IllegalArgumentException("Invalid input. First name, last "
+        + "name and education level cannot be null.");
+    }
+    
+    if (!npiLevel.matches("\\d{10")) {
+      throw new IllegalArgumentException("NPI levels cannot be negative and must have 10 digits.");
+    }
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.educationLevel = educationLevel;
+    this.npiLevel = npiLevel;
+  }
+
+  @Override
+  public String getFirstName() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public String getLastName() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public EducationLevel getEducationLevel() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
