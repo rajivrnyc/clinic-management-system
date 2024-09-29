d325587847b308e15359e4667a3735641132355d
rajiv <rajiv@DESKTOP-2RDGT1I>
2024-09-28 23:05:39 -0400

    implemented jobTitle and getJobTitle to Staff subclasses.

--- src/clinic/ClinicalStaff.java
+++ src/clinic/ClinicalStaff.java
@@ -5,6 +5,7 @@ package clinic;
  * This class inherits the staff class.
  */
 public class ClinicalStaff implements Staff {
+  private String jobTitle;
   private String firstName;
   private String lastName;
   private EducationLevel educationLevel;
@@ -13,12 +14,13 @@ public class ClinicalStaff implements Staff {
   /**
    * A constructor for the ClinicalStaff class that represents a clinical staff member.
    * 
+   * @param jobTitle The job title for the clinical staff member.
    * @param firstName The first name of the clinical staff member.
    * @param lastName The last name of the clinical staff member.
    * @param educationLevel The education level of the clinical staff member.
    * @param npiLevel The NPI level of the clinical staff member.
    */
-  public ClinicalStaff(String firstName, String lastName, EducationLevel 
+  public ClinicalStaff(String jobTitle, String firstName, String lastName, EducationLevel 
       educationLevel, String npiLevel) {
     if (firstName == null || lastName == null || educationLevel == null || npiLevel == null) {
       throw new IllegalArgumentException("Invalid input. First name, last "
@@ -28,10 +30,16 @@ public class ClinicalStaff implements Staff {
     if (!npiLevel.matches("\\d{10")) {
       throw new IllegalArgumentException("NPI levels cannot be negative and must have 10 digits.");
     }
+    this.jobTitle = jobTitle;
     this.firstName = firstName;
     this.lastName = lastName;
     this.educationLevel = educationLevel;
     this.npiLevel = npiLevel;
+  }
+  
+  @Override
+  public String getJobTitle() {
+    return this.jobTitle;
   }
 
   @Override
--- src/clinic/NonClinicalStaff.java
+++ src/clinic/NonClinicalStaff.java
@@ -6,6 +6,7 @@ package clinic;
  * interface.
  */
 public class NonClinicalStaff implements Staff {
+  private String jobTitle;
   private String firstName;
   private String lastName;
   private EducationLevel educationLevel;
@@ -16,21 +17,29 @@ public class NonClinicalStaff implements Staff {
    * Constructor for the NonClinicalStaff class  which represents a staff
    * member that is considered non clinical.
    * 
+   * @param jobTitle The job title of the non clinical staff member.
    * @param firstName The first name of the non clinical staff member.
    * @param lastName The last name of the non clinical staff member.
    * @param educationLevel The education level of the non clinical staff member.
    * @param cprLevel The CPR level of the non clinical staff member.
    */
-  public NonClinicalStaff(String firstName, String lastName, EducationLevel educationLevel, 
+  public NonClinicalStaff(String jobTitle, String firstName, 
+        String lastName, EducationLevel educationLevel, 
       CprLevel cprLevel) {
     if (firstName == null || lastName == null || educationLevel == null || cprLevel == null) {
       throw new IllegalArgumentException("The names, education level and cprLevel for "
         + "non clinical staff cannot be left null.");
     }
+    this.jobTitle = jobTitle;
     this.firstName = firstName;
     this.lastName = lastName;
     this.educationLevel = educationLevel;
     this.cprLevel = cprLevel;
+  }
+  
+  @Override
+  public String getJobTitle() {
+    return this.jobTitle;
   }
 
   @Override
