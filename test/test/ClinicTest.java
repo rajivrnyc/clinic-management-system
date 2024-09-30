a026692a0804cd992cfbd44b8d93645e5b2f97ee
rajiv <rajiv@DESKTOP-2RDGT1I>
2024-09-29 22:45:01 -0400

    Added junit test case as well as began ClinicTest class.

--- test/test/ClinicTest.java
+++ test/test/ClinicTest.java
@@ -1,14 +1,35 @@
 package test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
+import clinic.Clinic;
+import clinic.ClinicInterface;
+import clinic.ClinicalStaff;
+import clinic.CprLevel;
+import clinic.EducationLevel;
+import clinic.NonClinicalStaff;
+import clinic.Patient;
+import clinic.Room;
+import clinic.RoomInterface;
+import clinic.RoomType;
+import clinic.Staff;
+import clinic.StaffFactoryHelper;
+import org.junit.Before;
 import org.junit.Test;
 
+/**
+ * JUnit Test Class for all classes within clinic package.
+ */
 public class ClinicTest {
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+  private Clinic clinic;
+  private Room primaryWaitingRoom;
+	
+  @Before
+  public void setUp() {
+    primaryWaitingRoom = new Room(0, 0, 20, 20, RoomType.WAITING, "Primary Waiting Room");
+    clinic = new Clinic("Local Clinic", 5, 10, 20, primaryWaitingRoom);
+  }
 
 }
