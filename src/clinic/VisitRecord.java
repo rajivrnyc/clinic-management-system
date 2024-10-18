5460971630f3c0b40bf79c675bdc7727b83b2bdd
rajiv <rajiv@DESKTOP-2RDGT1I>
2024-10-18 15:14:49 -0400

    Completed constructor for visitRecord class.

--- src/clinic/VisitRecord.java
+++ src/clinic/VisitRecord.java
@@ -3,22 +3,32 @@ package clinic;
 import java.time.LocalDateTime;
 
 /**
- * Creates a Record of a patient upon their most recent
+ * Record of a patient upon their most recent
  * visit to the clinic stating their time of arrival, chief complaint
  * and their body temperature.
  */
 public class VisitRecord implements Record {
   private final LocalDateTime dateArrival;
   private final String complaint;
-  private final double tempeprature;
+  private final double temperature;
   
+  /**
+   * Creates an instance of a patient's visit record where datearrival is the patient's
+   * arrival to the clinic, complaint is the patient's chief complaint to the clinic,
+   * and temperature is the patient's body temperature in Celsius rounded to 1 decimal.
+   * 
+   * @param dateArrival the patient's arrival date and time to the clinic.
+   * @param complaint the patient's chief complaint upon coming to the clinic.
+   * @param temperature the patient's temperature in Celsius upon coming to the clinic.
+   */
   public VisitRecord(LocalDateTime dateArrival, String complaint, double temperature) {
-   
-    	
+    if (dateArrival == null || complaint == null) {
+      throw new IllegalArgumentException("Patient cannot have a null arrival or complaint.");
     }
+    
     this.dateArrival = dateArrival;
     this.complaint = complaint;
-    this.temp = temp;
+    this.temperature = temperature;
   }
 
   @Override
