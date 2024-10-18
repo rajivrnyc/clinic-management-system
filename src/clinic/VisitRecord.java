be00c7f30a9165c2ca844808979c46197ab9e49f
rajiv <rajiv@DESKTOP-2RDGT1I>
2024-10-18 14:43:26 -0400

    Added javadoc comment to VisitRecord class.

--- src/clinic/VisitRecord.java
+++ src/clinic/VisitRecord.java
@@ -3,7 +3,9 @@ package clinic;
 import java.time.LocalDateTime;
 
 /**
- * 
+ * Creates a Record of a patient upon their most recent
+ * visit to the clinic stating their time of arrival, chief complaint
+ * and their body temperature.
  */
 public class VisitRecord implements Record {
 
