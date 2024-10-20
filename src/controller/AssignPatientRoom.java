e7e1c7fb7d7497286a0607a2e6a4c6baa1478624
rajiv <rajiv@DESKTOP-2RDGT1I>
2024-10-20 15:44:07 -0400

    Created file for assigning a patient a room.

---
+++ src/controller/AssignPatientRoom.java
@@ -0,0 +1,17 @@
+package controller;
+
+import clinic.ClinicInterface;
+import controller.ClinicCommand;
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * Command for controller to assign a patient to a room.
+ */
+public class AssignPatientRoom implements ClinicCommand {
+
+  @Override
+  public void execute(ClinicInterface model, Scanner scanner) throws IOException {
+
+  }
+}
