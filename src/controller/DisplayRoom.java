c4ee037dae5629988b74c4b8d96fdbeb43d79889
rajiv <rajiv@DESKTOP-2RDGT1I>
2024-10-19 20:30:22 -0400

    completed room display.

--- src/controller/DisplayRoom.java
+++ src/controller/DisplayRoom.java
@@ -18,12 +18,15 @@ public class DisplayRoom implements ClinicCommand {
   @Override
   public void execute(ClinicInterface model, Scanner scanner) throws IOException {
     model.addNewPatient("Sally", "Johnson", "01/02/1990");
-    PatientInterface sallyJ = model.getPatients().get(model.getPatients().size() - 1);
+    model.addNewPatient("Joe", "Johnson", "01/02/1991");
+    PatientInterface sallyJ = model.getPatients().get(model.getPatients().size() - 2);
+    PatientInterface joeJ = model.getPatients().get(model.getPatients().size() - 1);
     model.addNewClinicalStaff("Doctor", "John", "Smith", EducationLevel.MASTERS, "0123456789");
     ClinicalStaffInterface john = (ClinicalStaffInterface) model.getEmployees().get(0);
     model.assignStaff(sallyJ, john);
     RoomInterface roomTest = model.getRoomFromNumber(2);
     model.assignPatient(sallyJ, roomTest);
+    model.assignPatient(joeJ, roomTest);
     System.out.println(roomTest);
   }
 }
