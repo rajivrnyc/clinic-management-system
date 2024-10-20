4f1611ce4bfd3c3a06de480c098167e0f0b01d36
rajiv <rajiv@DESKTOP-2RDGT1I>
2024-10-20 13:57:22 -0400

    Added command to controller for existing patient.

--- src/controller/ClinicController.java
+++ src/controller/ClinicController.java
@@ -39,6 +39,7 @@ public class ClinicController {
     knownCommands.put(3, s -> new DisplaySeatingChart());
     knownCommands.put(4, s -> new RegisterPatientCommand());
     knownCommands.put(5, s -> new RegisterNewClinStaff());
+    knownCommands.put(6, s -> new RegisterExistingPatient());
   }
   
   /**
