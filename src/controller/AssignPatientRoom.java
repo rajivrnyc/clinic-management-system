bf1f1207999cc1012005d61049a4cffc5a3c4099
rajiv <rajiv@DESKTOP-2RDGT1I>
2024-10-20 18:26:48 -0400

    Adding command for assign a patient to room to controller.

--- src/controller/AssignPatientRoom.java
+++ src/controller/AssignPatientRoom.java
@@ -1,6 +1,10 @@
 package controller;
 
 import clinic.ClinicInterface;
+import clinic.Patient;
+import clinic.PatientInterface;
+import clinic.Room;
+import clinic.RoomInterface;
 import controller.ClinicCommand;
 import java.io.IOException;
 import java.util.Scanner;
@@ -12,6 +16,16 @@ public class AssignPatientRoom implements ClinicCommand {
 
   @Override
   public void execute(ClinicInterface model, Scanner scanner) throws IOException {
-
+    System.out.println("Sally is a new patient");
+    RoomInterface room1 = model.getRoomFromNumber(1);
+    
+    System.out.println();
+    System.out.println(room1);
+    PatientInterface sally = new Patient(1, "Sally", "Johnson", "01/02/1990");
+    System.out.println();
+    System.out.println("Assigning Sally to room 1");
+    model.assignPatient(sally, room1);
+    System.out.println(room1);
+    
   }
 }
