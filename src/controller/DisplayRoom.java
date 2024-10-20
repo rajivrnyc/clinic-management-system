0f3a575feef1e3a3e0afe29667592facd00088b1
rajiv <rajiv@DESKTOP-2RDGT1I>
2024-10-19 20:21:23 -0400

    Added display room command class.

--- src/controller/DisplayRoom.java
+++ src/controller/DisplayRoom.java
@@ -1,6 +1,10 @@
 package controller;
 
 import clinic.ClinicInterface;
+import clinic.ClinicalStaffInterface;
+import clinic.EducationLevel;
+import clinic.PatientInterface;
+import clinic.RoomInterface;
 import controller.ClinicCommand;
 import java.io.IOException;
 import java.util.Scanner;
@@ -13,6 +17,13 @@ public class DisplayRoom implements ClinicCommand {
 
   @Override
   public void execute(ClinicInterface model, Scanner scanner) throws IOException {
-
+    model.addNewPatient("Sally", "Johnson", "01/02/1990");
+    PatientInterface sallyJ = model.getPatients().get(model.getPatients().size() - 1);
+    model.addNewClinicalStaff("Doctor", "John", "Smith", EducationLevel.MASTERS, "0123456789");
+    ClinicalStaffInterface john = (ClinicalStaffInterface) model.getEmployees().get(0);
+    model.assignStaff(sallyJ, john);
+    RoomInterface roomTest = model.getRoomFromNumber(2);
+    model.assignPatient(sallyJ, roomTest);
+    System.out.println(roomTest);
   }
 }
