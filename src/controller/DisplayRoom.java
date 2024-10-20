874bf98d1973064a14c5a103f7450e76a9f36d21
rajiv <rajiv@DESKTOP-2RDGT1I>
2024-10-19 20:09:43 -0400

    Added a command to display room info.

---
+++ src/controller/DisplayRoom.java
@@ -0,0 +1,18 @@
+package controller;
+
+import clinic.ClinicInterface;
+import controller.ClinicCommand;
+import java.io.IOException;
+import java.util.Scanner;
+
+
+/**
+ * Command used to display the contents of a room within a clinic model.
+ */
+public class DisplayRoom implements ClinicCommand {
+
+  @Override
+  public void execute(ClinicInterface model, Scanner scanner) throws IOException {
+
+  }
+}
