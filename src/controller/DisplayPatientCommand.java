fde5acb25d505c76fb92fe35b175761753a22e6c
rajiv <rajiv@DESKTOP-2RDGT1I>
2024-10-20 21:28:13 -0400

    changed display to selecting from a list of patients.

--- src/controller/DisplayPatientCommand.java
+++ src/controller/DisplayPatientCommand.java
@@ -8,6 +8,8 @@ import clinic.EducationLevel;
 import clinic.PatientInterface;
 import controller.ClinicCommand;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -17,16 +19,39 @@ public class DisplayPatientCommand implements ClinicCommand {
 	
   @Override
   public void execute(ClinicInterface model, Scanner scanner) throws IOException {
-    model.addNewPatient("Sally", "Johnson", "01/02/1990");
-    PatientInterface sallyJ = model.getPatients().get(model.getPatients().size() - 1);
-    model.addNewClinicalStaff("Doctor", "John", "Smith", EducationLevel.MASTERS, "0123456789");
-    model.addNewClinicalStaff("Nurse", "Steve", "Wilson", EducationLevel.MASTERS, "0123456789");
-    ClinicalStaffInterface john = (ClinicalStaffInterface) model.getEmployees().get(0);
-    ClinicalStaffInterface steve = (ClinicalStaffInterface) model.getEmployees().get(1);
-    sallyJ.addRecord("Headache", 39);
-    model.assignStaff(sallyJ, john);
-    model.assignStaff(sallyJ, steve);
-    System.out.println(sallyJ);
+    System.out.println("List of Patients:");
+    List<PatientInterface> patients = model.getPatients();
+    
+    if (patients.isEmpty()) {
+      System.out.println("Patient List is empty");
+      return;
+    }
+    for (int i = 0; i < patients.size(); i++) {
+      System.out.println((i + 1) + ": " + (patients.get(i)).getFirstName() + " " 
+          + (patients.get(i)).getLastName());
+    }
+    
+    int patientIndex = -1;
+    boolean running = true;
+    while (running) {
+      System.out.println();
+      System.out.print("Select patient to display: ");
+      scanner.nextLine();
+      try {
+        patientIndex = Integer.parseInt(scanner.nextLine()) - 1;
+        if (patientIndex < 0 || patientIndex >= patients.size()) {
+          throw new IllegalArgumentException("Invalid patient number.");
+        }
+        running = false;
+      } catch (NumberFormatException e) {
+        System.out.println("Invalid input. Please enter a valid patient number.");
+        return;
+      } catch (IllegalArgumentException e) {
+        System.out.println("Invalid input. Please enter a valid number.");
+        return;
+      }
+    }
+    System.out.println();
+    System.out.println(patients.get(patientIndex));  
   }
-
 }
