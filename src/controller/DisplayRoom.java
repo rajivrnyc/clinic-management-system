b6396eb6030e2a666a6baa538ba968e64c2411de
rajiv <rajiv@DESKTOP-2RDGT1I>
2024-10-20 21:44:30 -0400

    displays list of rooms for user to select from.

--- src/controller/DisplayRoom.java
+++ src/controller/DisplayRoom.java
@@ -7,6 +7,7 @@ import clinic.PatientInterface;
 import clinic.RoomInterface;
 import controller.ClinicCommand;
 import java.io.IOException;
+import java.util.List;
 import java.util.Scanner;
 
 
@@ -17,7 +18,32 @@ public class DisplayRoom implements ClinicCommand {
 
   @Override
   public void execute(ClinicInterface model, Scanner scanner) throws IOException {
-    RoomInterface roomTest1 = model.getRoomFromNumber(1);
-    System.out.println(roomTest1);
+    List<RoomInterface> rooms = model.getRooms();
+    
+    System.out.println("List of Rooms:");
+    for (int i = 0; i < rooms.size(); i++) {
+      System.out.println((i + 1) + ": " + rooms.get(i).getRoomName());
+    }
+    System.out.println();
+    int roomIndex = -1;
+    boolean running = true;
+    while (running) {
+      System.out.print("Select room to display: ");
+      scanner.nextLine();
+      System.out.println();
+      try {
+        roomIndex = Integer.parseInt(scanner.nextLine()) - 1;
+        if (roomIndex < 0 || roomIndex >= rooms.size()) {
+          throw new IllegalArgumentException("Invalid room number.");
+        }
+        running = false; 
+      } catch (NumberFormatException e) {
+        System.out.println("Invalid input. Please enter a valid room number.");
+      } catch (IllegalArgumentException e) {
+        System.out.println(e.getMessage());
+      }
+    }
+    System.out.println();
+    System.out.println(rooms.get(roomIndex).toString());
   }
 }
