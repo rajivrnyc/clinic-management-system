c9a52b2445ddd34df98bc627591625594ef34177
rajiv <rajiv@DESKTOP-2RDGT1I>
2024-11-13 21:57:47 -0500

    Began implementation of unassign ClinicalStaff

--- src/controller/UnassignClinicalStaff.java
+++ src/controller/UnassignClinicalStaff.java
@@ -1,8 +1,32 @@
 package controller;
 
+import clinic.ClinicInterface;
+import clinic.ClinicStaffAndPatientInfo;
+import clinic.PatientInterface;
+import java.io.IOException;
+import java.util.List;
+import java.util.Scanner;
+
+
+
 /**
  * Controller command to unassign a clinical Staff member from a patient.
  */
-public class UnassignClinicalStaff {
+public class UnassignClinicalStaff implements ClinicCommand {
 
+  @Override
+  public void execute(ClinicInterface model, Scanner scanner) throws IOException {
+    ClinicStaffAndPatientInfo tempModel = (ClinicStaffAndPatientInfo) model;
+    System.out.println("Select a Patient:");
+    List<PatientInterface> patients = tempModel.getPatients();
+    
+    if (patients.isEmpty()) {
+      System.out.println("Patient List is empty");
+      return;
+    }
+    
+    for (int i = 0; i < patients.size(); i++) {
+      
+    }
+  }
 }
