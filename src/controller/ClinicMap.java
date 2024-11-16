8e87dd57463f2924e6ce5e33c2c14350ea00ece7
rajiv <rajiv@DESKTOP-2RDGT1I>
2024-11-16 17:37:25 -0500

    added graphics, creating background for my clinic map

--- src/controller/ClinicMap.java
+++ src/controller/ClinicMap.java
@@ -1,7 +1,13 @@
 package controller;
 
 import clinic.ClinicStaffAndPatientInfo;
+import clinic.PatientInterface;
+import clinic.RoomInterface;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.List;
 import java.util.Scanner;
 
 
@@ -13,5 +19,16 @@ public class ClinicMap implements ClinicCommand2 {
 
   @Override
   public void execute(ClinicStaffAndPatientInfo model, Scanner scanner) throws IOException {
+    List<RoomInterface> rooms = model.getRooms();
+    List<PatientInterface> patients = model.getPatients();
+    
+    int width = 800;
+    int height = 600;
+    
+    BufferedImage map = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+    Graphics g = map.getGraphics();
+    
+    g.setColor(Color.white);
+    g.fillRect(0, 0, width, height);
   }
 }
