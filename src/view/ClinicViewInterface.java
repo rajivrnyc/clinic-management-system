56be0ff60eb62799023465275f73a478f0437c02
rajiv <rajiv@DESKTOP-2RDGT1I>
2024-12-03 14:40:41 -0500

    implemented basic view methods

--- src/view/ClinicViewInterface.java
+++ src/view/ClinicViewInterface.java
@@ -7,4 +7,46 @@ import java.awt.event.ActionListener;
  */
 public interface ClinicViewInterface {
 
+  /**
+   * Makes the view visible to the user.
+   */
+  void makeVisible();
+  
+  
+  /**
+   * Adds a listener for inputs from the user.
+   * 
+   * @param listener the action listener
+   */
+  void addCommandListener(ActionListener listener);
+  
+  
+  /**
+   *  An action to quit the program.
+   * 
+   * @param listener a listener for the quit button
+   */
+  void addQuitListener(ActionListener listener);
+  
+  
+  /**
+   * Displays a message in the view display area.
+   * 
+   * @param message the message being displayed
+   */
+  void showMessage(String message);
+  
+  
+  /**
+   * Clears the view of displayed messages.
+   */
+  void clear();
+  
+  
+  /**
+   * Closes the view and resets.
+   */
+  void exit();
+
+
 }
