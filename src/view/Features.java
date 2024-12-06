9550ce72b894d3502e546e749194a9620cba7431
rajiv <rajiv@DESKTOP-2RDGT1I>
2024-12-06 02:55:06 -0500

    added methods to features interface

--- src/view/Features.java
+++ src/view/Features.java
@@ -4,5 +4,48 @@ package view;
  * The interface represents the features that this program offers.
  */
 public interface Features {
-
+  
+  /**
+   * Loads a clinic text file into memory and updates the view.
+   * 
+   */
+  void loadClinicFile();
+  
+  /**
+   * Clears all the records in the memory.
+   */
+  void clearAllRecords();
+  
+  /**
+   * Exits program.
+   */
+  void exitProgram();
+  
+  /**
+   * Register's a new patient into the clinic and records their visit.
+   */
+  void registerNewPatient();
+  
+  /**
+   * Assigns a clinical staff member to a patient.
+   */
+  void assignStafftoPatient();
+  
+  /**
+   * Displays a patient's info.
+   */
+  void displayPatientInfo();
+  
+  
+  /**
+   * Unassigns a staff member from a patient.
+   */
+  void unassignStaffFromPatient();
+  
+  /**
+   * Sends a patient home from the clinic.
+   */
+  void sendPatientHome();
+  
+  
 }
