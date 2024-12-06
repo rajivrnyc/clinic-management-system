0ace556ce3292e9aa7f71a5b45894953ecc9a2cf
rajiv <rajiv@DESKTOP-2RDGT1I>
2024-12-05 23:02:03 -0500

    Making a gui rollback controller

---
+++ src/controller/ClinicControllerGUI.java
@@ -0,0 +1,15 @@
+package controller;
+
+import view.MasterViewInterface;
+
+/**
+ * Controller to support view implementation for the mvc.
+ */
+public class ClinicControllerGui extends ClinicController2{
+  private final MasterViewInterface view;
+  
+  public ClinicControllerGui(MasterViewInterface view) {
+    
+  }
+
+}
