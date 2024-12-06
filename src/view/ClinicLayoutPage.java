e34cfa145fca87274dba30bdce74d804d6a80a7f
rajiv <rajiv@DESKTOP-2RDGT1I>
2024-12-06 17:25:39 -0500

    implememnted the jpanel for the room.

--- src/view/ClinicLayoutPage.java
+++ src/view/ClinicLayoutPage.java
@@ -1,16 +1,22 @@
 package view;
 
 import clinic.ClinicStaffAndPatientInfo;
+import clinic.PatientInterface;
 import clinic.Room;
+import clinic.RoomInterface;
 import java.awt.BorderLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionListener;
 import java.util.List;
 import java.util.Map;
+import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
+import javax.swing.DefaultListModel;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.SwingConstants;
 
 
@@ -32,7 +38,26 @@ public class ClinicLayoutPage extends JPanel implements ClinicViewInterface {
     JPanel roomsPanel = new JPanel();
     roomsPanel.setLayout(new BoxLayout(roomsPanel, BoxLayout.Y_AXIS));
     
-    List<Room> rooms = clinic.getRooms();
+    List<RoomInterface> rooms = clinic.getRooms();
+    
+    for (RoomInterface room : rooms) {
+      JPanel roomPanel = new JPanel();
+      roomPanel.setLayout(new BorderLayout());
+      roomPanel.setBorder(BorderFactory.createTitledBorder(room.getRoomName()));
+      
+      List<PatientInterface> patients = room.getResidents();
+      
+      DefaultListModel<String> patientListModel = new DefaultListModel<>();
+      for (PatientInterface patient : patients) {
+        patientListModel.addElement(patient.getFirstName() + " " 
+            + patient.getLastName());
+      }
+      JList<String> patientList = new JList<>(patientListModel);
+      
+      roomPanel.add(new JScrollPane(patientList), BorderLayout.CENTER);
+      roomsPanel.add(roomPanel);
+    }
+    this.add(new JScrollPane(roomsPanel), BorderLayout.CENTER);
   }
   
   @Override
