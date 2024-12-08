4ac85c76ab92682fbd54a52a8472cbc937ed325d
rajiv <rajiv@DESKTOP-2RDGT1I>
2024-12-08 14:45:46 -0500

    Made new model interface for reset method to clear fields

---
+++ src/clinic/ClinicInterface3.java
@@ -0,0 +1,13 @@
+package clinic;
+
+/**
+ * Represents a clinic model with new features relating to 
+ * functions to be used uniquely by the view.
+ */
+public interface ClinicInterface3 extends ClinicStaffAndPatientInfo {
+  
+  /**
+   * resets the fields of the model.
+   */
+  void reset();
+}
