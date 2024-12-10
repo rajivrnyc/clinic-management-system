c601b5029028414197c5f565dde561ed93b906bc
rajiv <rajiv@DESKTOP-2RDGT1I>
2024-12-09 20:56:47 -0500

    made gui controller cleaner

--- src/controller/ClinicControllerGui.java
+++ src/controller/ClinicControllerGui.java
@@ -115,77 +115,12 @@ public class ClinicControllerGui extends ClinicController2 implements Features {
 
   @Override
   public void sendPatientHome() {
-    clinicLayoutPage = view.getLayoutPage();
-    JOptionPane.showMessageDialog(null, "Please select a patient to send home.");
-    clinicLayoutPage.enablePatientSelectionSendHome(this);
+    SendHomeViewInterface sendHome = new SendHomeView(this.model, this.view);
+    sendHome.execute();
   }
   
   @Override
   public void go() {
     view.makeVisible();
   }
-  
-  private void setModel(ClinicInterface3 newModel) {
-    this.model = newModel;
-    view.refresh(newModel);
-  }
-  
-  
-  @Override
-  public void processPatientSendHome(PatientInterface patient) {
-    clinicLayoutPage.disablePatientSelection();
-    selectedPatient = patient;
-    JOptionPane.showMessageDialog(null, "Please select the approving staff member.");
-    
-    List<ClinicalStaffInterface2> clin = getClinStaff();
-    String[] staffNames  = new String[clin.size()];
-    for (int i = 0; i < clin.size(); i++) {
-      if (clin.get(i).getStatus()) {
-        StringBuilder sb = new StringBuilder();
-        sb.append(clin.get(i).getFirstName()).append(" ").append(clin.get(i).getLastName());
-        staffNames[i] = sb.toString();
-      }
-    }
-    JComboBox<String> staffCombo = new JComboBox<>(staffNames);
-    int result = JOptionPane.showConfirmDialog(null, staffCombo, "Select Clinical Staff",
-        JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
-    String selectedStaffName = "";
-    if (result == JOptionPane.OK_OPTION) {
-      selectedStaffName = (String) staffCombo.getSelectedItem();
-    }
-    for (ClinicalStaffInterface2 c : clin) {
-      StringBuilder sb = new StringBuilder();
-      sb.append(c.getFirstName()).append(" ").append(c.getLastName());
-      String fullName = sb.toString();
-      if (fullName.equals(selectedStaffName)) {
-        selectedStaff = c;
-      }
-    }
-    try {
-      model.sendHome(selectedPatient, selectedStaff);
-      JOptionPane.showMessageDialog(null, "Patient sent home successfully.");
-      setModel(model);
-    } catch (IllegalArgumentException e) {
-      StringBuilder sb = new StringBuilder();
-      sb.append("Error: ").append(e.getMessage());
-      JOptionPane.showMessageDialog(null, sb.toString());
-    }
-    
-  }
-  
-  private List<ClinicalStaffInterface2> getClinStaff() {
-    List<ClinicalStaffInterface2> clin = new ArrayList<>();
-    List<Staff> staff = model.getEmployees();
-    List<StaffClass> stfClass = new ArrayList<>();
-    for (Staff emp : staff) {
-      stfClass.add((StaffClass) emp);
-    }
-    for (StaffClass stf : stfClass) {
-      if (stf.isClinicalStaff()) {
-        clin.add((ClinicalStaffInterface2) stf);
-      }
-    }
-    return clin;
-  }
-
 }
