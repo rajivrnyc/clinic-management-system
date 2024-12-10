1991ee969a2e0bc867445ea27b63554be6b8af51
rajiv <rajiv@DESKTOP-2RDGT1I>
2024-12-09 20:43:20 -0500

    sucessfully tested refactor of unassign

--- src/controller/ClinicControllerGui.java
+++ src/controller/ClinicControllerGui.java
@@ -108,9 +108,8 @@ public class ClinicControllerGui extends ClinicController2 implements Features {
 
   @Override
   public void unassignStaffFromPatient() {
-    clinicLayoutPage = view.getLayoutPage();
-    JOptionPane.showMessageDialog(null, "Please select a patient to unassign from");
-    clinicLayoutPage.enablePatientSelectionUnassignStaff(this);
+    UnassignStaffViewInterface unassignStaff = new UnassignStaffView(this.model, this.view);
+    unassignStaff.execute();
   }
   
 
@@ -131,50 +130,6 @@ public class ClinicControllerGui extends ClinicController2 implements Features {
     view.refresh(newModel);
   }
   
-  
-  @Override
-  public void processPatientUnassignStaff(PatientInterface patient) {
-    clinicLayoutPage.disablePatientSelection();
-    selectedPatient = patient;
-    List<ClinicalStaffInterface> clin = selectedPatient.getAllocated();
-    if (clin.isEmpty()) {
-      JOptionPane.showMessageDialog(null, "This patient has no clinical staff members assigned");
-      return;
-    }
-    String[] staffNames  = new String[clin.size()];
-    for (int i = 0; i < clin.size(); i++) {
-      if (clin.get(i).getStatus()) {
-        StringBuilder sb = new StringBuilder();
-        sb.append(clin.get(i).getFirstName()).append(" ").append(clin.get(i).getLastName());
-        staffNames[i] = sb.toString();
-      }
-    }
-    JComboBox<String> staffCombo = new JComboBox<>(staffNames);
-    int result = JOptionPane.showConfirmDialog(null, staffCombo, "Select Clinical Staff",
-        JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
-    String selectedStaffName = "";
-    if (result == JOptionPane.OK_OPTION) {
-      selectedStaffName = (String) staffCombo.getSelectedItem();
-    }
-    
-    for (ClinicalStaffInterface c : clin) {
-      StringBuilder sb = new StringBuilder();
-      sb.append(c.getFirstName()).append(" ").append(c.getLastName());
-      String fullName = sb.toString();
-      if (fullName.equals(selectedStaffName)) {
-        selectedPatientStaff = c;
-      }
-    }
-    try {
-      model.unassignClinStaff(selectedPatientStaff, selectedPatient);
-      JOptionPane.showMessageDialog(null, "Clinical Staff Member Unassigned");
-      setModel(model);
-    } catch (IllegalArgumentException e) {
-      StringBuilder sb = new StringBuilder();
-      sb.append("Error: ").append(e.getMessage());
-      JOptionPane.showMessageDialog(null, sb.toString());
-    }
-  }
   
   @Override
   public void processPatientSendHome(PatientInterface patient) {
